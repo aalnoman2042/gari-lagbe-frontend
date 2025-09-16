a9bce916dd66b978b6703074ed4135791dee1e1f
aalnoman2042 <abdullahalnoman2042@gmail.com>
2025-09-17 01:04:53 +0600

    pwa implement

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,41 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Gari-Lagbe",
+        short_name: "G-L",
+        description: "A Ride sharing Application",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        icons: [
+          {
+            src: "icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+
+
+  ],
   
   resolve: {
     alias: {
@@ -14,3 +45,4 @@ export default defineConfig({
   },
   
 })
+
